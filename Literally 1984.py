83ab0fd06f0ffa2ed68a649dd2eb0ff690d401c1
CarterzCode <theotherdude42069@gmail.com>
2026-09-18 09:44:28 -0600

    Add literally 1984

---
+++ Literally 1984.py
@@ -0,0 +1,20 @@
+"""
+Checks if it is 1984 big brother
+Carter Quarles - September 2026
+"""
+
+def main() -> None:
+  # input
+  year: int = int(input())
+
+  # processing
+  if year == 1984:
+    # output
+    print("Literally 1984")
+  else:
+    print("Not 1984... yet")
+
+
+if __name__ == "__main__":
+  main()
+    
